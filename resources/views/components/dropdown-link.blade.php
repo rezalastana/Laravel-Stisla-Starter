b6ec87ff13a265d240abafad1a4bd6c0dd6d7128
rezalastana <rezalastana21@gmail.com>
2023-04-11 11:34:10 +0700

    laravel9-stisla-starter

---
+++ resources/views/components/dropdown-link.blade.php
@@ -0,0 +1,2 @@
+<a {{ $attributes->merge(['class' => 'block w-full text-left text-sm text-gray-700 hover:bg-gray-100
+    focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out']) }}>{{ $slot }}</a>
